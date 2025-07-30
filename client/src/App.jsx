39dd7d91289bb0788e6772a0dfbf58f20b2fcf2b
Noma7code <henryah.yeb.5@gmail.com>
2025-07-30 20:01:36 +0000

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const navigate = useNavigate();
+  return (
+    <header className=" bg-amber-400  shadow-md ">
+      <div className="flex justify-between items-center max-w-6xl p-4 mx-auto">
+        <h1 className="text-sm sm:text-3xl font-bold flex flex-wrap">
+          <span className="text-slate-500 ">Fancy</span>
+          <span className="text-slate-700 ">Estates</span>
+        </h1>
+        <form className="bg-slate-100 rounded-lg p-2 flex justify-center items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className=" bg-transparent outline-0 w-30 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <nav className="flex gap-3 hover:cursor-pointer list-none">
+          <Link to="/">
+            <li className="hidden sm:inline hover:text-slate-700">Home</li>
+          </Link>
+
+          <Link to="/about">
+            <li className="hidden sm:inline hover:text-slate-700">About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="sm:inline hover:text-slate-700">Sign in</li>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
