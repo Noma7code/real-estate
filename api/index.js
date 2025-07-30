4bca848099bcfc780a7552ad5ac87eaf35bef3be
Noma7code <henryah.yeb.5@gmail.com>
2025-07-30 22:49:57 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
 const PORT = process.env.PORT;
