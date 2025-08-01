f9e7d3b486f961eb4d30d340d8236fa85e9bd483
Noma7code <henryah.yeb.5@gmail.com>
2025-08-01 08:27:24 +0000

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 const { userModel } = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
 const { errorHandler } = require("../utils/error");
 
+//signup
 async function signup(req, res, next) {
   const { username, email, password } = req.body;
   try {
@@ -22,6 +25,41 @@ async function signup(req, res, next) {
   }
 }
 
+//signin
+async function signin(req, res, next) {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+    const validUser = await userModel.findOne({ email });
+
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPasword = await bcrypt.compare(password, validUser.password);
+    if (!validPasword) return next(errorHandler(401), "Wrong Credentials");
+
+    //token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    //remove psd
+    const { password: pass, ...restUserInfo } = validUser._doc;
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: Date.now() + 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json(restUserInfo);
+  } catch (error) {
+    return next(error);
+  }
+}
+
 module.exports = {
   signup,
+  signin,
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { signup } = require("../controllers/auth.controller");
+const { signup, signin } = require("../controllers/auth.controller");
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
-
+authRouter.post("/signin", signin);
 module.exports = {
   authRouter,
 };
