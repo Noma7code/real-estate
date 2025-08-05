27b7a5f22a077e0f46b2d72e758035e1e4ac58d7
Noma7code <henryah.yeb.5@gmail.com>
2025-08-05 09:49:42 +0000

    complete upload listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,91 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [formData, setFormData] = useState({
+    imgUrls: [],
+  });
+
+  const handleImageSubmit = async () => {
+    setUploading(true);
+    setImageUploadError(false);
+    if (!files || files.length === 0) {
+      setImageUploadError("No file selected");
+      return;
+    }
+
+    if (files.length > 0 && files.length + formData.imgUrls.length < 7) {
+      try {
+        const uploadPromises = files.map((file) => storeImage(file));
+        const imageUrls = await Promise.all(uploadPromises);
+
+        console.log("Uploaded URLs:", imageUrls);
+
+        // You can now use imageUrls in your form state
+        setFormData((prev) => ({
+          ...prev,
+          imgUrls: imageUrls,
+        }));
+        setUploading(false);
+        setImageUploadError(false);
+      } catch (error) {
+        setImageUploadError("Image upload faied (2m per image)");
+        setUploading(false);
+      }
+    } else {
+      setImageUploadError("You can only upload 6 images per listing");
+      setUploading(false);
+    }
+  };
+
+  //signature for secure upload
+  async function getSignaturForUpload(folder) {
+    try {
+      const res = await axios.post("/api/upload/sign-upload", { folder });
+      return res.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const storeImage = async (file) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        // Get signed timestamp and signature from your backend
+        const { timestamp, signature } = await getSignaturForUpload("images");
+
+        const data = new FormData();
+        data.append("file", file);
+        data.append("timestamp", timestamp);
+        data.append("signature", signature);
+        data.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY);
+        data.append("folder", "images");
+
+        const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+        const resourceType = "image";
+        const api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`;
+
+        const res = await axios.post(api, data, { withCredentials: false });
+        const { secure_url } = res.data;
+
+        resolve(secure_url);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+        reject(error);
+      }
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imgUrls: formData.imgUrls.filter((_, i) => i !== index),
+    });
+  };
+
   return (
     <main className="mx-auto p-3 max-w-4xl ">
       <h1 className="text-center my-7 text-3xl font-semibold">
@@ -78,7 +163,7 @@ export default function CreateListing() {
                 required
                 className="border border-gray-300 rounded-lg p-3"
               />
-              <label htmlFor="bathhouse text-center">Baths</label>
+              <label htmlFor="bathhouse ">Baths</label>
             </div>
             <div className="flex items-center gap-2">
               <input
@@ -125,17 +210,42 @@ export default function CreateListing() {
             <input
               type="file"
               id="images"
-              accept="images/*"
+              accept="image/*"
               multiple
               className="p-3 border border-gray-300 rounded w-full "
+              onChange={(e) => setFiles(Array.from(e.target.files))}
             />
             <button
+              disabled={uploading}
+              type="button"
+              onClick={handleImageSubmit}
               className="p-3 text-green-700 border border-green-700 rounded
              uppercase hover:shadow-lg disabled:opacity-80"
             >
-              Upload
+              {uploading ? "Uploading" : "upload"}
             </button>
           </div>
+          <p className="text-red-700">{imageUploadError}</p>
+          {formData.imgUrls.length > 0 &&
+            formData.imgUrls.map((url, index) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 shadow-2xl shadow-gray-300 items-center"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-30 h-20 object-cover rounded-lg"
+                />
+                <button
+                  type="button"
+                  onClick={() => handleRemoveImage(index)}
+                  className="text-red-700 uppercase hover:opacity-75 hover:font-bold"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="text-white uppercase bg-green-500 rounded-lg text-center p-3 hover:opacity-85">
             Create Listing
           </button>
