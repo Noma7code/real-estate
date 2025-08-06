1e06ceef6a507329fa7693f3a50e91a84d138cae
Noma7code <henryah.yeb.5@gmail.com>
2025-08-06 15:37:56 +0000

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -26,7 +26,28 @@ async function deleteListing(req, res, next) {
   }
 }
 
+async function updateListing(req, res, next) {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json("Listing has been updated successfully");
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   createListing,
   deleteListing,
+  updateListing,
 };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   createListing,
   deleteListing,
+  updateListing,
 } = require("../controllers/listing.controller");
 const { verifyUser } = require("../utils/verifyUser");
 
@@ -9,6 +10,7 @@ const listingRouter = express.Router();
 
 listingRouter.post("/create", verifyUser, createListing);
 listingRouter.delete("/delete/:id", verifyUser, deleteListing);
+listingRouter.post("/update/:id", verifyUser, updateListing);
 module.exports = {
   listingRouter,
 };
