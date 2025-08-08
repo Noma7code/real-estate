e83840a7816341e079bb5f0cda9c24c841ee2f7d
Noma7code <henryah.yeb.5@gmail.com>
2025-08-08 08:42:00 +0000

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 export default function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className=" bg-amber-400  shadow-md ">
       <div className="flex justify-between items-center max-w-6xl p-4 mx-auto">
@@ -13,13 +32,20 @@ export default function Header() {
           <span className="text-slate-500 ">Fancy</span>
           <span className="text-slate-700 ">Estates</span>
         </h1>
-        <form className="bg-slate-100 rounded-lg p-2 flex justify-center items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 rounded-lg p-2 flex justify-center items-center"
+        >
           <input
+            onChange={(e) => setSearchTerm(e.target.value)}
             type="text"
             placeholder="Search..."
+            value={searchTerm}
             className=" bg-transparent outline-0 w-30 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <nav className="flex gap-3 hover:cursor-pointer list-none">
           <Link to="/">
